e633c6eb9745e38ec1ba093c5039456539e4afad
Lazzatti Lucas <lazzattilucas06@gmail.com>
2025-09-10 21:01:48 -0300

    Revert "lord give me strength"
    This reverts commit a96218f59328659eba50ced5b14379ab15b0f1bc.

--- Software/MagicAlonso_STM/lib/qre_array/qre_array.c
+++ Software/MagicAlonso_STM/lib/qre_array/qre_array.c
@@ -41,7 +41,6 @@ static void adc1_setup_once(void) {
 
     // Clave en F1 para disparo por software (SWSTART):
     adc_enable_external_trigger_regular(ADC1, ADC_CR2_EXTSEL_SWSTART);
-    adc_external_trigger_regular_enable(ADC1);  // <-- habilita EXTTRIG
 
     done = true;
 }
